d2aed9f954a08e3bf942ede3428372b2685e90db
darialoghin <daria.loghin139@yahoo.com>
2023-01-16 23:39:27 +0200

    Add project files.

---
+++ Loghin_Daria_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Manichiura;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Loghin_Daria_Lab7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Manichiura;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
